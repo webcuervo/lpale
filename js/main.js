cd8331a8e1205de275cc8a28e79df8eca3608812
Esteban Cuervo <webcuervo@gmail.com>
2022-09-02 14:38:18 -0300

    card, texto y nav

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ window.addEventListener("scroll", function() {
     let header = document.getElementById("header")
     header.classList.toggle("sticky", window.scrollY > 0)
 })
+
+window.addEventListener("scroll", function() {
+    let texto = document.getElementsById("texto")
+    texto.classList.toggle("none", window.scrollY > 0)
+})
