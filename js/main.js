891dcc7a3d5f6614848e1ef8dd1a827bc0e13b60
Esteban Cuervo <webcuervo@gmail.com>
2022-09-06 15:02:49 -0300

    cambios de fondo

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scrollY > 0)
 })
 
-window.addEventListener("scroll", function() {
+/* window.addEventListener("scroll", function() {
     let texto = document.getElementsById("texto")
     texto.classList.toggle("none", window.scrollY > 0)
-})
+}) */
+
